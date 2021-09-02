e59743a726be083fae404cda6986dd9d7afa80f9
Kriska939 <cristinacorrado93@gmail.com>
2021-09-02 15:50:04 +0200

    added all albums

--- js/script.js
+++ js/script.js
@@ -3,10 +3,20 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#app',
     data: {
+        albumList: "",
 
     },
     methods: {
 
+    },
 
+    created() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((r) => {
+                const response = r.data.response;
+                this.albumList = response;
+                console.log(this.albumList);
+            });
     }
+
 });
